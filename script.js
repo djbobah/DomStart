f9f60d0470cb4421d55aa4f82137a398ef87d648
Vladimir Kirpichev <djbobah@mail.ru>
2024-01-06 12:02:55 +0300

    done

--- script.js
+++ script.js
@@ -43,35 +43,9 @@ function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-// const colors = [
-//   { rgb: "255,0,0", value: "красный" },
-//   { rgb: "255,255,0", value: "желтый" },
-//   { rgb: "0,255,0", value: "зеленый" },
-//   { rgb: "66,170,255", value: "голубой" },
-//   { rgb: "0,0,255", value: "синий" },
-//   { rgb: "255,192,203", value: "розовый" },
-//   { rgb: "128,128,128", value: "серый" },
-//   { rgb: "0,0,0", value: "черный" },
-//   { rgb: "255,255,255", value: "белый" },
-// ];
-const colorsCbfRainbowRGB = {
-  violet: [120, 28, 129],
-  indigo: [64, 67, 153],
-  blue: [72, 139, 194],
-  green: [107, 178, 140],
-  olive: [159, 190, 87],
-  yellow: [210, 179, 63],
-  orange: [231, 126, 49],
-  red: [217, 33, 32],
-};
-function rgb2(values) {
-  return "rgb(" + values.join(", ") + ")";
-}
-console.log(rgb2(colorsCbfRainbowRGB.violet));
 const task2 = document.querySelector(".task2");
 
 task2.style.display = "flex";
-// task2.style.flexWrap = "wrap";
 task2.style.alignContent = "flex-center";
 task2.style.alignItems = "center";
 task2.style.justifyContent = "center";
@@ -79,7 +53,8 @@ const r = getRandom(256, 0);
 const g = getRandom(256, 0);
 const b = getRandom(256, 0);
 const rgb = `rgb(${r},${g},${b})`;
-console.log(rgb);
+let n_match = ntc.name(rgbToHex(r, g, b));
+
 const temp2 = document.createElement("div");
 r + g + b > 765 / 2
   ? (temp2.style.color = "black")
@@ -94,12 +69,5 @@ temp2.style.justifyContent = "center";
 temp2.style.alignItems = "center";
 temp2.innerHTML += `Формат цвета RGB: ${rgb}`;
 temp2.innerHTML += `<br>Формат цвета HEX: ${rgbToHex(r, g, b)}`;
-temp2.innerHTML += `<br>Формат цвета текстовый: ${rgb}`;
+temp2.innerHTML += `<br>Формат цвета текстовый: ${n_match[1]}`;
 task2.appendChild(temp2);
-
-var n_match = ntc.name(rgbToHex(r, g, b));
-n_rgb = n_match[0]; // RGB value of closest match
-n_name = n_match[1]; // Text string: Color name
-n_exactmatch = n_match[2]; // True if exact color match
-
-console.log(n_rgb, n_name);
